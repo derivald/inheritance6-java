b5d04993a55054f16979cea9065df9f619a1e689
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-01-17 23:14:44 -0300

    first commit

---
+++ courseClassesAbstratas/src/application/App.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class App {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        List<Account> list = new ArrayList<>();
+
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+
+        double sum = 0.0;
+        for (Account account : list) {
+            sum += account.getBalance();
+        }
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account account : list) {
+            account.deposit(10.0);
+        }
+        for (Account account : list) {
+            System.out.printf("Update balance for account %d: %.2f%n", account.getNumber(), account.getBalance());
+        }
+
+    }
+}
